b266e841923f9a4e90a8287ba45e51617f8e9ebf
languo <lan.guo14@gmail.com>
2017-05-23 18:29:38 -0700

    debugged plotlite.js

--- plotlite.js
+++ plotlite.js
@@ -1,23 +1,13 @@
 
-/* This function parses PlotLite format strings and output plotly format strings.
+/*
+This function parses PlotLite format strings and output plotly format strings.
 PlotList format guide:
 The first line should start with a chart type keyword, followed by the chart title.
 The second line starts with the xData keyword, followed by data to be plotted on the x-axis.
 The third line starts with the yData keyword, followed by data to be plotted on the y-axis.
 For chart type, currently supporting scatter, bar plots.
+
 */
-
-
-function prepopulateExample() {
-  let examplePlotLite = `
-  scatter My awesome scatter plot
-  xData 1,2,3,4,5,6,7,8
-  yData 2,4,6,8,10,12,14,16
-`;
-  let exampleContainer = document.getElementById('plotliteCode');
-  exampleContainer.value = examplePlotLite;
-}
-
 
 function parsePlotLine(plotLine) {
   const plotLineEndOfType = plotLine.indexOf(' ');
@@ -27,8 +17,8 @@ function parsePlotLine(plotLine) {
 }
 
 function parseData(dataLine) {
-  const dim = dataLine.match(/[xyz](?!Data)/); // matches x,y,or z followed by 'Data'
-  const data = dataLine.match(/[-*.*0-9]+|\s(?!,)/g); // globally matches any signed or unsigned numbers and spaces followed by ',' 
+  const dim = dataLine.match(/[xyz](?=Data)/); // matches x,y,or z followed by 'Data'
+  const data = dataLine.match(/[-*.*0-9]+|\s(?=,)/g); // globally matches any signed or unsigned numbers and spaces followed by ','
   if (!dim) {
     return {dim: null, data: null};
   }
@@ -69,6 +59,7 @@ function parsePlotlite(input) {
 
   const plotLine = lines[0];
   ({plotType, plotTitle} = parsePlotLine(plotLine));
+
   if (plotType !== 'scatter' && plotType !== 'bar') {
     errorMessage = 'Undefined chart type! Supported chart types: scatter, bar.';
     return {
@@ -80,12 +71,12 @@ function parsePlotlite(input) {
   }
 
   let dataLines = lines.slice(1); // an Array of lines(strings) containing data
-  dataLines = dataLines.filter(entry => entry.trim() != ''); // throw away empty lines
-  console.log(dataLines);
+  dataLines = dataLines.filter(entry => entry.trim() !== ''); // throw away empty lines
+
   for (let dataLine of dataLines) {
-    let dim, data;
+    let dim, data; // dim stores the dimension of the data, e.g. 'x' or 'y'
     ({dim, data} = parseData(dataLine));
-    console.log(dim, data);
+    // console.log(dim, data);
     if (!dim || !data) {
       errorMessage = 'Please provide xData and yData for plotting.';
       return {
@@ -109,8 +100,8 @@ function parsePlotlite(input) {
 }
 
 function generatePlotlyPlot(plotType, plotTitle, dataObject, outputDiv) {
-  const xData = dataObject.xData;
-  const yData = dataObject.yData;
+  const xData = dataObject.x;
+  const yData = dataObject.y;
 
   var layout = {
     title: plotTitle,
@@ -134,73 +125,12 @@ function plotliteToPlotly() {
   let input = document.getElementById('plotliteCode').value;
   let outputDiv = document.getElementById('plotly');
   let {plotType, plotTitle, dataObject, errorMessage} = parsePlotlite(input);
-  console.log(plotType, plotTitle, dataObject);
 
   if (errorMessage) {
     outputDiv.innerHTML = errorMessage;
   }
   else {
-    //generatePlotlyPlot(plotType, plotTitle, dataObject, outputDiv);
-    outputDiv.innerHTML = [plotType,plotTitle,errorMessage].join('-');
-    console.log(dataObject);
+    generatePlotlyPlot(plotType, plotTitle, dataObject, outputDiv);
   }
 }
 
-
-
-  /*
-  if (!input) {
-    var output = 'Please input PlotLite code.';
-  }
-  else {
-    let lines = input.split('\n');
-    if (lines.length < 3) {
-      output = 'Insuffient inputs. Please refer to format guide for input formatting!';
-    }
-    else {
-      const plotLine = lines[0].trim();
-      const plotType, plotTitle = parsePlotLine(plotLine);
-
-      if (plotType === 'scatter' || plotType === 'bar') {
-        const xDataLine = lines[1];
-        const yDataLine = lines[2];
-        const xData = xDataLine.split(/,\s*|\s+/).filter(Number);
-        const yData = yDataLine.split(/,\s*|\s+/).filter(Number);
-        if ((xData.length === 0) || (yData.length === 0)) {
-          output = 'Please input both x and y data.';
-        }
-        else {
-          output =
-          `
-          var layout = {
-            title: '${plotTitle}',
-            autosize: true,
-            width: 500,
-            height: 300,
-            margin: {
-            t: 100, b: 0, l: 0, r: 0
-            }
-          };
-
-          Plotly.plot(this.div, [{
-            x: [${xData}],
-            y: [${yData}],
-            type: '${plotType}' }],
-            layout,
-            {displayModeBar: false});
-          `;
-        }
-      }
-      else {
-	output = 'Undefined chart type! Please declare your chart type in the first line of your input. Supported chart types: scatter, bar #to add more later';
-      }
-    }
-  }
-  return output;
-}
-
-function plotliteTranslate() {
-  const x = document.getElementById('plotliteCode').value;
-  document.getElementById('plotly').innerHTML = plotliteToPlotly(x);
-}
-*/
